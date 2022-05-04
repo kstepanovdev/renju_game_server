ab0db078ff68db1d288b499cd5b43cd4146cb842
Kyrylo Stepanov <k.stepanov@tutanota.com>
2022-05-04 19:21:27 +0300

    added: basic tokio; ERROR: tokio's TcpStream cannot be cloned

---
+++ src/game.rs
@@ -0,0 +1,171 @@
+use std::net::IpAddr;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug)]
+struct Player {
+   ip: IpAddr,
+   name: String,
+   color: Option<usize>,
+}
+
+impl Player {
+   fn new(name: String, ip: IpAddr) -> Self {
+       Player {
+           name,
+           color: None,
+           ip,
+       }
+   }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Connect { username: String },
+    Move { move_id: usize, username: String },
+    Reset,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ServerResponse {
+    Ok { player_ip: IpAddr },
+    Fail { message: String, player_ip: IpAddr },
+    Move { move_id: usize, color: usize, winner: Option<String> },
+    Reset,
+}
+
+pub struct Game {
+  pub players: Vec<Player>,
+  pub active_player: Option<usize>,
+  pub winner: Option<String>,
+  pub field: [usize; 255],
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Game {
+            players: vec![],
+            active_player: None,
+            winner: None,
+            field: [0; 255],
+        }
+    }
+
+    fn reset(&mut self) {
+        self.active_player = None;
+        self.winner = None;
+        self.field = [0; 255];
+    }
+
+    fn winner_check(&mut self, player_id: usize) {
+        self.horizontal_check(player_id);
+        [14, 15, 16].map(|shift| self.shift_check(player_id, shift));
+    }
+
+    fn horizontal_check(&mut self, player_id: usize) {
+        let player = &self.players[player_id];
+
+        let rows = self.field.chunks(15);
+        for row in rows {
+            let mut win_line = vec![];
+            let mut idx = 0;
+            while idx < row.len() {
+                let cell_color = row[idx];
+                if cell_color == player.color.unwrap() {
+                    win_line.push(idx);
+                } else {
+                    win_line = vec![];
+                }
+                if win_line.len() >= 5 {
+                    self.winner = Some(player.name.clone());
+                    return;
+                }
+                idx += 1;
+            }
+        }
+    }
+
+    fn shift_check(&mut self, player_id: usize, shift: usize) {
+        let player = &self.players[player_id];
+        let mut idx = 0;
+        let mut win_line = vec![];
+        let winner_color = player.color.unwrap();
+        while idx < self.field.len() {
+            if self.field[idx] != winner_color {
+                idx += 1;
+                win_line = vec![];
+                continue;
+            }
+            win_line.push(idx);
+            let mut i = idx;
+            while i + shift < self.field.len() && self.field[i + shift] == winner_color {
+                win_line.push(i);
+                if win_line.len() >= 5 {
+                    self.winner = Some(player.name.clone());
+                    return;
+                }
+                i += shift;
+            }
+            win_line = vec![];
+            idx += 1;
+        }
+    }
+    
+    pub fn handle_action(&mut self, data: &[u8], player_ip: IpAddr) -> ServerResponse {
+        let data = bincode::deserialize::<Command>(data);
+        match data {
+            Ok(Command::Reset) => {
+                self.reset();
+                ServerResponse::Reset
+            }
+
+            Ok(Command::Connect { username }) => {
+                let new_player = Player::new(username, player_ip);
+                self.players.push(new_player);
+                ServerResponse::Ok { player_ip }
+            }
+
+            Ok(Command::Move { move_id, username }) => {
+                if self.players.len() < 2 {
+                    return ServerResponse::Fail {
+                        message: "Wait for a second player to connect".to_string(),
+                        player_ip,
+                    };
+                }
+
+                let (player_id, second_player_id) = if self.players[0].name == username {
+                    (0_usize, 1_usize)
+                } else {
+                    (1_usize, 0_usize)
+                };
+
+                match self.active_player {
+                    Some(active_player_id) => {
+                        if player_id != self.active_player.unwrap() {
+                            return ServerResponse::Fail { message: "It's not your move".to_string(), player_ip };
+                        };
+
+                        if active_player_id == 0_usize {
+                            self.active_player = Some(1)
+                        } else {
+                            self.active_player = Some(0)
+                        };
+                    }
+                    None => {
+                        self.players[player_id].color = Some(1_usize);
+                        self.players[second_player_id].color = Some(2_usize);
+
+                        self.active_player = Some(second_player_id);
+                    }
+                }
+                let color = self.players[player_id].color.unwrap();
+                self.field[move_id] = color;
+                self.winner_check(player_id);
+
+                ServerResponse::Move { move_id, color, winner: self.winner.clone() }
+            }
+            Err(e) => {
+                panic!("{}", e)
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,186 +1,21 @@
 use core::{panic, time};
 use std::collections::HashMap;
 use std::io::{stdin, Read, Write};
-use std::net::{IpAddr, TcpListener, TcpStream};
+use std::net::{IpAddr, };
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, RwLock};
 use std::thread::sleep;
 
 use serde::{Deserialize, Serialize};
-use threadpool::ThreadPool;
 
-#[derive(Debug)]
-struct Player {
-    ip: IpAddr,
-    name: String,
-    color: Option<usize>,
-}
+use tokio::net::{TcpListener, TcpStream};
 
-impl Player {
-    fn new(name: String, ip: IpAddr) -> Self {
-        Player {
-            name,
-            color: None,
-            ip,
-        }
-    }
-}
+mod game;
+use game::{ServerResponse, Game};
+use tokio::sync::oneshot;
 
-#[derive(Serialize, Deserialize, Debug)]
-enum GameAction {
-    Connect(String),
-    Move(usize, String),
-    Reset,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum ServerResponse {
-    Ok(IpAddr),
-    Fail(String, IpAddr),
-    Move(usize, usize, Option<String>),
-    Reset,
-}
-
-struct Game {
-    players: Vec<Player>,
-    active_player: Option<usize>,
-    winner: Option<String>,
-    field: [usize; 255],
-}
-
-impl Game {
-    fn new() -> Self {
-        Game {
-            players: vec![],
-            active_player: None,
-            winner: None,
-            field: [0; 255],
-        }
-    }
-
-    fn reset(&mut self) {
-        self.active_player = None;
-        self.winner = None;
-        self.field = [0; 255];
-    }
-
-    fn winner_check(&mut self, player_id: usize) {
-        self.horizontal_check(player_id);
-        [14, 15, 16].map(|shift| self.shift_check(player_id, shift));
-    }
-
-    fn horizontal_check(&mut self, player_id: usize) {
-        let player = &self.players[player_id];
-
-        let rows = self.field.chunks(15);
-        for row in rows {
-            let mut win_line = vec![];
-            let mut idx = 0;
-            while idx < row.len() {
-                let cell_color = row[idx];
-                if cell_color == player.color.unwrap() {
-                    win_line.push(idx);
-                } else {
-                    win_line = vec![];
-                }
-                if win_line.len() >= 5 {
-                    self.winner = Some(player.name.clone());
-                    return;
-                }
-                idx += 1;
-            }
-        }
-    }
-
-    fn shift_check(&mut self, player_id: usize, shift: usize) {
-        let player = &self.players[player_id];
-        let mut idx = 0;
-        let mut win_line = vec![];
-        let winner_color = player.color.unwrap();
-        while idx < self.field.len() {
-            if self.field[idx] != winner_color {
-                idx += 1;
-                win_line = vec![];
-                continue;
-            }
-            win_line.push(idx);
-            let mut i = idx;
-            while i + shift < self.field.len() && self.field[i + shift] == winner_color {
-                win_line.push(i);
-                if win_line.len() >= 5 {
-                    self.winner = Some(player.name.clone());
-                    return;
-                }
-                i += shift;
-            }
-            win_line = vec![];
-            idx += 1;
-        }
-    }
-}
-
-fn handle_game_action(data: &[u8], game: Arc<RwLock<Game>>, player_ip: IpAddr) -> ServerResponse {
-    let data = bincode::deserialize::<GameAction>(data);
-    match data {
-        Ok(GameAction::Reset) => {
-            game.write().unwrap().reset();
-            ServerResponse::Reset
-        }
-
-        Ok(GameAction::Connect(name)) => {
-            let new_player = Player::new(name, player_ip);
-            let mut game = game.write().unwrap();
-            game.players.push(new_player);
-            ServerResponse::Ok(player_ip)
-        }
-
-        Ok(GameAction::Move(move_id, name)) => {
-            if game.read().unwrap().players.len() < 2 {
-                return ServerResponse::Fail(
-                    "Wait for a second player to connect".to_string(),
-                    player_ip,
-                );
-            }
-
-            let mut game = game.write().unwrap();
-            let (player_id, second_player_id) = if game.players[0].name == name {
-                (0_usize, 1_usize)
-            } else {
-                (1_usize, 0_usize)
-            };
-
-            match game.active_player {
-                Some(active_player_id) => {
-                    if player_id != game.active_player.unwrap() {
-                        return ServerResponse::Fail("It's not your move".to_string(), player_ip);
-                    };
-
-                    if active_player_id == 0_usize {
-                        game.active_player = Some(1)
-                    } else {
-                        game.active_player = Some(0)
-                    };
-                }
-                None => {
-                    game.players[player_id].color = Some(1_usize);
-                    game.players[second_player_id].color = Some(2_usize);
-
-                    game.active_player = Some(second_player_id);
-                }
-            }
-            let player_color = game.players[player_id].color.unwrap();
-            game.field[move_id] = player_color;
-            game.winner_check(player_id);
-
-            ServerResponse::Move(move_id, player_color, game.winner.clone())
-        }
-        Err(e) => {
-            panic!("{}", e)
-        }
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     tracing_subscriber::fmt::init();
     println!("Enter desired IP or leave it blank to keep a default value:");
     let mut buffer = String::new();
@@ -196,84 +31,60 @@ fn main() {
             panic!("{}", e)
         }
     };
-    let listener = TcpListener::bind(address).unwrap();
-    listener
-        .set_nonblocking(true)
-        .expect("Failed to initialize non-blocking");
+    let listener = TcpListener::bind(address).await.unwrap();
     println!("Server listening on ip:port = {}", address);
 
     let game = Arc::new(RwLock::new(Game::new()));
-    let mut clients: HashMap<IpAddr, TcpStream> = HashMap::new();
-    let pool = ThreadPool::new(4);
+    let mut clients: HashMap<IpAddr, Receiver<ServerResponse>> = HashMap::new();
+    let (server_tx, server_rx): (Sender<ServerResponse>, Receiver<ServerResponse>) = channel();
+        loop {
+            if let Ok((socket, addr)) = listener.accept().await {
+                println!("Client {:?} connected", socket.peer_addr());
+                let (client_tx, client_rx) = oneshot::channel();
+                clients.insert(addr.ip(), client_rx);
 
-    let (tx, rx): (Sender<ServerResponse>, Receiver<ServerResponse>) = channel();
-    loop {
-        if let Ok((mut stream, _socket_addr)) = listener.accept() {
-            println!("Client {:?} connected", stream.peer_addr());
+                let game = Arc::clone(&game);
+                let tx = server_tx.clone();
 
-            let player_ip = stream.peer_addr().unwrap().ip();
-            clients.insert(player_ip, stream.try_clone().unwrap());
+                tokio::spawn(async move {
+                    process(socket, game, tx).await
+                });
+        };
 
-            let game = Arc::clone(&game);
-            // let mut stream_clone = stream.unwrap().try_clone();
-            let tx = tx.clone();
-            pool.execute(move || loop {
-                let arc_game = Arc::clone(&game);
-
-                let mut data = [0; 64];
-                match stream.read(&mut data) {
-                    Ok(size) => {
-                        if size == 0 {
-                            return;
-                        }
-
-                        let response = handle_game_action(&data, arc_game, player_ip);
-                        tracing::error!("{:?}", response);
-                        if let Err(e) = tx.send(response) {
-                            tracing::error!("Sending message to a transmitter failed: {}", e)
-                        };
-                    }
-                    Err(e) => {
-                        println!("Data read error: {}", e);
-                    }
-                }
-                sleep(time::Duration::from_millis(300));
-            });
-        }
-        match rx.try_recv() {
+        match server_rx.try_recv() {
             Ok(response) => match response {
-                ServerResponse::Move(move_id, player_color, winner) => {
-                    let resp =
-                        bincode::serialize(&ServerResponse::Move(move_id, player_color, winner))
-                            .unwrap();
-                    for mut client in clients.values() {
+                ServerResponse::Move { move_id, color, winner } => {
+                    let resp = bincode::serialize(&ServerResponse::Move {
+                        move_id,
+                        color,
+                        winner
+                    }).unwrap();
+                    for (client,  mut socket) in clients {
                         tracing::error!("{:?}", resp);
-                        client.write_all(&resp).unwrap();
+                        socket.write_all(&resp);
                     }
                 }
                 ServerResponse::Reset => {
                     let resp = bincode::serialize(&ServerResponse::Reset).unwrap();
-                    for mut client in clients.values() {
+                    for (addr, mut socket) in clients {
                         tracing::error!("{:?}", resp);
-                        client.write_all(&resp).unwrap();
+                        socket.write_all(&resp);
                     }
                 }
-                ServerResponse::Ok(player_ip) => {
-                    let resp = bincode::serialize(&ServerResponse::Ok(player_ip)).unwrap();
+                ServerResponse::Ok { player_ip } => {
+                    let resp = bincode::serialize(&ServerResponse::Ok { player_ip }).unwrap();
                     clients
                         .get_mut(&player_ip)
                         .unwrap()
-                        .write_all(&resp)
-                        .unwrap();
+                        .write_all(&resp);
                 }
-                ServerResponse::Fail(message, player_ip) => {
+                ServerResponse::Fail { message, player_ip } => {
                     let resp =
-                        bincode::serialize(&ServerResponse::Fail(message, player_ip)).unwrap();
+                        bincode::serialize(&ServerResponse::Fail { message, player_ip }).unwrap();
                     clients
                         .get_mut(&player_ip)
                         .unwrap()
-                        .write_all(&resp)
-                        .unwrap();
+                        .write_all(&resp);
                 }
             },
             Err(e) => {
@@ -281,5 +92,27 @@ fn main() {
             }
         }
         sleep(time::Duration::from_millis(100));
+    }
+}
+
+async fn process(mut socket: TcpStream, game: Arc<RwLock<Game>>, tx: Sender<ServerResponse>) {
+    let mut data = [0; 64];
+    let player_ip = socket.peer_addr().unwrap().ip();
+    socket.readable().await;
+    match socket.try_read(&mut data) {
+        Ok(size) => {
+            if size == 0 {
+                return;
+            }
+
+            let response = game.write().unwrap().handle_action(&data, player_ip);
+            tracing::error!("{:?}", response);
+            if let Err(e) = tx.send(response) {
+                tracing::error!("Sending message to a transmitter failed: {}", e)
+            };
+        }
+        Err(e) => {
+            println!("Data read error: {}", e);
+        }
     }
 }
